f89fd676b71f508b0fd8fc22b409c006b424a5f3
Beatriz Blank <blankbeatriz@gmail.com>
2018-04-19 21:08:10 -0400

    class

--- class_work/jquery_city/js/app.js
+++ class_work/jquery_city/js/app.js
@@ -1,39 +1,14 @@
 // 1. Create .click() handlers for each of the thumbnails: #first, #second, #third, #fourth
 // 2. Use .attr() to change the `src` attribute of #bigimage to correspond to image that was clicked
 
-$("#first").click(displayFirst)
-$("#second").click(displaySecond)
-$("#third").click(displayThird)
-$("#fourth").click(displayFourth)
+
+$(".thumb").click(changeImg)
 
 
-function displayFirst() {
-
-	$("#bigimage").attr("src", "img/1.jpg")
+function changeImg(event) {
 
 
+    var newSrc = $(event.currentTarget).attr("src");
+	$("#bigimage").attr("src", newSrc)
 }
 
-function displaySecond(event) {
-
-console.log(event.currentTarget);
-
-    var newSrc = $(eventTarget)
-	$("#bigimage").attr("src", "img/2.jpg")
-}
-
-
-
-function displayThird() {
-
-	$("#bigimage").attr("src", "img/3.jpg");
-
-
-}
-
-function displayFourth() {
-
-	$("#bigimage").attr("src", "img/4.jpg");
-
-
-}
