2bd77b6f8954c5bb054dfacf53ae1d5e5477d276
Beatriz Blank <blankbeatriz@gmail.com>
2018-04-26 21:18:47 -0400

    c

--- homework/cash_register/js/app.js
+++ homework/cash_register/js/app.js
@@ -2,6 +2,17 @@
 // When I enter a number into the input and submit
 // I expect the new entry to appear in the table
 
+$("#total").change(newEntry);
+
+function newEntry () {
+
+	var valueEntered = $("#newEntry").val();
+
+	$("#total").html("valueEntered");
+
+	console.log(valueEntered)
+}
+
 // As a user
 // When I enter a number into the input and submit
 // I expect the total to be updated as the sum all of the entries
