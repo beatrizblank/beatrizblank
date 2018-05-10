3602a17241fc386e8f665dc4db8adbb48a2f0f62
Beatriz Blank <blankbeatriz@gmail.com>
2018-05-10 18:48:54 -0400

    class

---
+++ class_work/responsive_navbar/js/app.js
@@ -0,0 +1,15 @@
+// As a User
+// When I see the nav bar on desktop
+// There is no burger (only Home/About/Contact/Blog)
+
+// As a User
+// When I see the nav bar on mobile
+// I see a burger
+
+// As a User
+// When I click the burger
+// I see the nav appear below the header
+
+// As a User
+// When I click the burger again
+// I see the nav hide
