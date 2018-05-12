681ea8747afad4bb24098883f68cb5d23c0f2992
Beatriz Blank <blankbeatriz@gmail.com>
2018-05-12 13:06:18 -0400

    JS is finally working!

--- Final Project/JS/script.js
+++ Final Project/JS/script.js
@@ -1,1 +1,22 @@
-script.js
+
+function calculateEggs() {
+
+  console.log("HEY")
+	var eggsPrice = 900.000;
+
+	var salaryInput = $("#salary").val();
+
+	var numberEggs = (salaryInput / eggsPrice);
+console.log(numberEggs)
+	$("#total-num-of-eggs-output").html(numberEggs);
+
+}
+
+$("#click-me").click(calculateEggs)
+
+
+
+
+//*As a user, when a get a result the background img changes (Maybe depending on the results)
+
+
