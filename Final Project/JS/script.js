f62ca437abba2d46e38e34c6f3952c001f566c26
Beatriz Blank <blankbeatriz@gmail.com>
2018-05-17 18:42:08 -0400

    fp

--- Final Project/JS/script.js
+++ Final Project/JS/script.js
@@ -9,36 +9,25 @@ function architectsOutput () {
 
 }
 
+$("#professions").click(changes);
 
-$("#professions").click(changeBackground);
+function changes (){
 
-function changeBackground() {
-
-	$("body").css("background-color", "white");
-	$("body").css("color", "black");
-	$(".percentage").removeClass("grey");
-	$(".percentage").addClass("yellow");
-	$("#professions").removeClass("grey");
-	$("#professions").addClass("yellow");
-
-}
-
-
-$("#professions").click(removeSalary);
-
-function removeSalary () {
  $("#output").empty();
-}
-
-
-$("#output").animate({
+ $("#output").animate({
     "font-size":"20px",
-  }, 6000) 
-
+  }, 5500) 
 $(".groceries").animate({
     "font-size":"50px",
-  }, 6000) 
+  }, 5500) 
+$("body").css("background-color", "white");
+$("body").css("color", "black");
+$(".percentage").removeClass("grey");
+$(".percentage").addClass("yellow");
+$("#professions").removeClass("grey");
+$("#professions").addClass("yellow");
 
+}
 
 
 //*As a user, when a get a result the background img changes (Maybe depending on the results)
