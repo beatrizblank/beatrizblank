6dd825a9d18214f4dd960f20d147eb89ffe3811e
Beatriz Blank <blankbeatriz@gmail.com>
2018-05-17 13:37:34 -0400

    finalproject

--- Final Project/JS/script.js
+++ Final Project/JS/script.js
@@ -1,12 +1,25 @@
  
 $("#professions").change(architectsOutput);
 
-
-
 function architectsOutput () {
  
  var professionSalary = $("#professions").val();
  $("#output").append(professionSalary);
+ $(".show-info").show("slow");
+
+}
+
+
+$("#professions").click(changeBackground);
+
+function changeBackground() {
+
+	$("body").css("background-color", "white");
+	$("body").css("color", "black");
+	$(".percentage").removeClass("grey");
+	$(".percentage").addClass("yellow");
+	$("#professions").removeClass("grey");
+	$("#professions").addClass("yellow");
 
 }
 
@@ -15,12 +28,12 @@ $("#professions").click(removeSalary);
 
 function removeSalary () {
  $("#output").empty();
-
 }
+
 
 $("#output").animate({
     "font-size":"30px",
-  }, 5000)
+  }, 5000) 
 
 
 
